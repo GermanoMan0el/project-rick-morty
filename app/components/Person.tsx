68dd8f2e160dac9176b12426c3f8f505e479fb61
Germano Manoel De Melo <germanomanoel007@gmail.com>
2025-11-10 22:15:40 -0300

    base do projeto

---
+++ app/components/Person.tsx
@@ -0,0 +1,8 @@
+export function Person() {
+    return(
+        <div className="bg-black p-1 flex flex-col items-center rounded-[5px] shadow-xl/20 ">
+            <img className="h-40" src="https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p10376284_b_v13_ac.jpg" alt="image" />
+            <span className="text-[15px] italic">Rick Morty</span>
+        </div>
+    );
+}
